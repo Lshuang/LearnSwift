e11962875b7b7a8f13c5a332860911248d4b69f6
LShawn <730356947@qq.com>
2015-08-12 17:33:30 +0800

    Core Location MapKit

--- WorkOutApp/WorkOutApp/DetailViewController.swift
+++ WorkOutApp/WorkOutApp/DetailViewController.swift
@@ -7,7 +7,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 class DetailViewController: UITableViewController {
+    @IBOutlet weak var playerView: UIView!
+    @IBOutlet weak var detailText: UILabel!
+    var workOut: WorkOut!
+    let playerVars = ["playsinline":"1"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = workOut.color
+        self.detailText.text = workOut.workOutText
+        self.title = workOut.title
+        self.navigationController?.navigationBar.barTintColor = workOut.color
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+    }
+    
+    
 
 }
--- WorkOutApp/WorkOutApp/ViewController.swift
+++ WorkOutApp/WorkOutApp/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    let manager = WorkOutDataSource()
+    var workOuts = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.workOuts = manager.getWorkOuts()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.workOuts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let workOut = self.workOuts[indexPath.row] as? WorkOut
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as? WorkOutCell
+        cell?.textCellLabel.text = workOut?.title
+        cell?.backgroundColor = workOut?.color
+        cell?.countLabel.text = "\(indexPath.row)"
+        cell?.selectionStyle = UITableViewCellSelectionStyle.None
+        return cell!
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "detailView") {
+            let cell = sender as? WorkOutCell
+            let indexPath = tableView.indexPathForCell(cell!)
+            let nvc = segue.destinationViewController as? UINavigationController
+            if let tmp = workOuts[indexPath!.row] as? WorkOut {
+                let dvc = nvc?.topViewController as! DetailViewController
+                dvc.workOut = tmp
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 55, 0)
+    }
 }
 
